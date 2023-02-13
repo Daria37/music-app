3e5a73107123bf2023332114932cd05ab961e451
daria <104294828+Daria37@users.noreply.github.com>
2023-02-13 19:13:55 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -8,17 +8,23 @@ let albums = [
             {
                 title: `Houndin`,
                 author: `Layto`,
-                time: `3:15`
+                time: `3:15`,
+                src: `music/Houndin.mp3`,
+                isPlaying: false
             },
             {
                 title: `All the Drinks`,
                 author: `Walking On Cars`,
-                time: `3:41`
+                time: `3:41`,
+                src: `music/All_the_Drinks.mp3`,
+                isPlaying: false
             },
             {
                 title: `New Kings`,
                 author: `Sleeping Wolf`,
-                time: `3:54`
+                time: `3:54`,
+                src: `music/New_Kings.mp3`,
+                isPlaying: false
             },
         ]
     },
@@ -31,17 +37,23 @@ let albums = [
             {
                 title: `Maybe yes`,
                 author: `Fitz & The Tantums`,
-                time: `2:40`
+                time: `2:40`,
+                src: `music/Maybe_Yes.mp3`,
+                isPlaying: false
             },
             {
                 title: `East of Eden`,
                 author: `Zella Day`,
-                time: `3:07`
+                time: `3:07`,
+                src: `music/East_of_Eden.mp3`,
+                isPlaying: false
             },
             {
                 title: `Legendary`,
                 author: `Welshly Arms`,
-                time: `3:50`
+                time: `3:50`,
+                src: `music/Legendary.mp3`,
+                isPlaying: false
             },
         ]
     },
@@ -54,17 +66,23 @@ let albums = [
             {
                 title: `Saviour`,
                 author: `Daisy Gray`,
-                time: `3:11`
+                time: `3:11`,
+                src: `music/Saviour.mp3`,
+                isPlaying: false
             },
             {
                 title: `Darkside`,
                 author: `Neoni`,
-                time: `2:48`
+                time: `2:48`,
+                src: `music/Darkside.mp3`,
+                isPlaying: false
             },
             {
                 title: `Winner`,
                 author: `Walgrove`,
-                time: `3:15`
+                time: `3:15`,
+                src: `music/Winner.mp3`,
+                isPlaying: false
             },
         ]
     },
@@ -77,17 +95,23 @@ let albums = [
             {
                 title: `Fake Love`,
                 author: `Shale Shake Go`,
-                time: `3:31`
+                time: `3:31`,
+                src: `music/Fake_Love.mp3`,
+                isPlaying: false
             },
             {
                 title: `CHAMPION`,
                 author: `Bishop Briggs`,
-                time: `2:53`
+                time: `2:53`,
+                src: `music/CHAMPION.mp3`,
+                isPlaying: false
             },
             {
                 title: `All My Friends`,
                 author: `The Revivalists`,
-                time: `3:38`
+                time: `3:38`,
+                src: `music/All_My_Friends.mp3`,
+                isPlaying: false
             },
         ]
     },
