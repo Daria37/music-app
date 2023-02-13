e3e9d056670654fe79fe080f1cf4e5b8fb345f16
daria <104294828+Daria37@users.noreply.github.com>
2023-02-13 19:15:04 +0300

    Update album.js

--- js/album.js
+++ js/album.js
@@ -37,14 +37,76 @@ if(!album){
     for(let j = 0; j < tracks.length; j++) {
         let track = tracks[j];
         playlist.innerHTML += `
-        <li class="list-group-item d-flex align-items-center">
-            <img src="assets/play.png" alt="" class="me-3" height="30px">
+        <li class="track list-group-item d-flex align-items-center">
+            <img class="img-play me-3" src="assets/play.png" alt="" height="30px">
+            <img class="img-pause me-3 d-none"src="assets/pause.png" alt="" height="30px">
             <div>
                 <div>${track.title}</div>
                 <div class="text-secondary">${track.author}</div>
             </div>
-            <div class="ms-auto">${track.time}</div>
+            <div class="container-progress">
+                <div class="progress-inner">
+                <div class="progress"></div>
+                </div>
+            </div>
+            <div class="time ms-auto">${track.time}</div>
+            <audio class="audio" src="${track.src}"></audio>
         </li>
                     `
+    }
+
+    function setupAudio() {
+        let trackNodes = document.querySelectorAll(`.track`); 
+        for (let i = 0; i < trackNodes.length; i++) { 
+            let track = tracks[i];
+            let node = trackNodes[i];
+            let timeNode = node.querySelector(`.time`);
+            let imgPause = node.querySelector(`.img-pause`);
+            let imgPlay = node.querySelector(`.img-play`);
+            let progress = node.querySelector(`.progress`);
+            let audio = node.querySelector(`.audio`); 
+
+            node.addEventListener(`click`, function () {
+                if (track.isPlaying) {
+                    track.isPlaying = false;
+                    audio.pause();
+                    imgPause.classList.add(`d-none`);
+                    imgPlay.classList.remove(`d-none`);
+                } else {
+                    track.isPlaying = true;
+                    audio.play();
+                    imgPause.classList.remove(`d-none`);
+                    imgPlay.classList.add(`d-none`);
+                    
+                    updateProgress();
+                }
+            });
+            function updateProgress() {
+                let time = getTime(audio.currentTime);
+                if(timeNode.innerHTML != time){
+                    timeNode.innerHTML = time;
+                    progress.style.width = (audio.currentTime * 100) / audio.duration + '%';
+                }
+                if (track.isPlaying) {
+                      requestAnimationFrame(updateProgress);
+                }
+                
+              }
+        }
+    }
+    setupAudio()
+
+    function getTime(time) {
+        let currentSeconds = Math.floor(time);
+        let minutes = Math.floor(currentSeconds / 60);
+        let seconds = Math.floor(currentSeconds % 60);
+
+        if(minutes < 10){
+            minutes = `0` + minutes;
+        }
+        if(seconds < 10){
+            seconds = `0` + seconds;
+        }
+        return `${minutes}:${seconds}`
     }
 }
